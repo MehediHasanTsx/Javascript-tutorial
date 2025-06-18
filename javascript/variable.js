714afa2acf2bd7873152ed16d2b8ecb116f05f25
Mehedi <mehedi.hasan.tsx@gmail.com>
2025-06-18 23:33:53 +0600

    variable

--- javascript/variable.js
+++ javascript/variable.js
@@ -10,9 +10,43 @@
 let a = 50; //if you know the exact value of variable
 let age = 20;
 let salary = 2000;
+let price =  "100";
+let cgpa = 3.5;
 
-
+// above shows variable value in the console
 console.log(a);
 console.log(age);
+console.log(price);
 console.log(salary);
 
+console.log(`You have obtained cgpa ${cgpa} out of 4.oo`);
+console.log(`Your salary is $${salary} taka per day`)
+
+
+//datatype of the variables shows in console by above written
+console.log(typeof a);
+console.log(typeof age);
+console.log(typeof salary);
+console.log(typeof price);
+console.log(typeof cgpa);
+
+
+// above are strings
+let Name = "Mehedi";
+let Favoritefood = "Apple"
+let gmail = "mehedihasan2003sr@gmail.com"
+
+console.log(Name);
+console.log(typeof Name);
+console.log(`My name is ${Name} Hasan`);
+
+console.log(Favoritefood);
+console.log(typeof Favoritefood);
+console.log(`My favorite food is ${Favoritefood}`);
+
+console.log(gmail);
+console.log(typeof gmail);
+console.log(`My gmail address is ${gmail}`);
+
+
+// above are booleans
