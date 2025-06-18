8ca4df25bccbb35d37f42c05e8e10c8dfc8bf21f
Mehedi <mehedi.hasan.tsx@gmail.com>
2025-06-18 22:51:27 +0600

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+// console.log(`Hello world`);
+// console.log(`I like apple`);
+
+// window.alert(`This is an alert!`);
+// window.alert(`I like apple!`);
+
+// This is a comment
+
+document.getElementById("heading_1").textContent = `Hello`;
+
+document.getElementById("para_1").textContent = `The quick brown fox jumps over the lazy dog.`
---
+++ javascript/variable.js
@@ -0,0 +1,18 @@
+// variable = A container that stores a value.
+// Behaves as if it were the value it contains.
+
+// 1. declaration  let x;
+// 2. assignment   x = 100;
+
+// let a;
+// a = 50; // if you dont know exact value of variable first,, then you can do like this
+
+let a = 50; //if you know the exact value of variable
+let age = 20;
+let salary = 2000;
+
+
+console.log(a);
+console.log(age);
+console.log(salary);
+
