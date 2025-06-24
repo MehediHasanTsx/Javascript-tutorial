198f7b931f5c62594a59f7a401e3a7e5288105e9
Mehedi <mehedi.hasan.tsx@gmail.com>
2025-06-24 21:14:56 +0600

    user_input

---
+++ javascript/4.user_input.js
@@ -0,0 +1,17 @@
+// How to accept user input
+
+// 1. Easy way = window prompt
+// 2. Professional way = html textbox
+
+
+// window prompt:
+// let username = window.prompt("What's your username?");
+// console.log(username);
+
+// Professional way:
+let username;
+document.getElementById("cmdbtn").onclick = function(){
+username = document.getElementById("cmdtext").value;
+ document.getElementById("cmdh1").textContent = `Hello ${username}`
+ console.log(username);
+}
